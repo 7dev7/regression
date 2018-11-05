d46db3b46b95bb85fe2e25ab0cc93096357a2f3d
Anton Glushkov <alexandrovich.anton7@gmail.com>
2018-11-05 12:16:11 +0400

    added file uploading

--- dataloading/views/load.py
+++ dataloading/views/load.py
@@ -1,7 +1,28 @@
 from django.views import generic
+from django.shortcuts import redirect, render_to_response
+from django.utils.datastructures import MultiValueDictKeyError
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.context_processors import csrf
 
 
 class LoadView(LoginRequiredMixin, generic.TemplateView):
     redirect_field_name = None
     template_name = 'load.html'
+
+    def post(self, request):
+        file = extract_file(request)
+        if file is not None:
+            print(file)
+            return redirect('/', request)
+        else:
+            args = {}
+            args.update(csrf(request))
+            args['error'] = 'Пожалуйста, загрузите файл'
+            return render_to_response('load.html', args)
+
+
+def extract_file(request):
+    try:
+        return request.FILES['inputFile']
+    except MultiValueDictKeyError:
+        return None
