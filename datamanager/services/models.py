854b2befc0ab7cee3282dd7f8856e7dc13a2c65f
Anton Glushkov <alexandrovich.anton7@gmail.com>
2019-03-16 07:00:17 +0400

    models naming

--- datamanager/services/models.py
+++ datamanager/services/models.py
@@ -3,14 +3,24 @@ from datamanager.models import MlModel, Dataset
 
 def save_ml_model(in_cols, out_cols, ds_id, model, degree, user):
     ds = Dataset.objects.get(pk=ds_id)
-    name = generate_model_name(in_cols, out_cols, model)
+    name = generate_model_name(in_cols, out_cols, model, degree)
     ml_model = MlModel(author=user, name=name, ds_in_cols=in_cols, ds_out_cols=out_cols,
                        dataset=ds, model=model, degree=degree)
     ml_model.save()
 
 
-def generate_model_name(in_cols, out_cols, model):
-    return 'Модель {}: {} -> {}'.format(model, in_cols, out_cols)
+def generate_model_name(in_cols, out_cols, model, degree):
+    degree_suffix = ', {} степени'.format(degree) if degree is not None else ''
+
+    return '{}: {} -> {}{}'.format(map_model(model), in_cols, out_cols, degree_suffix)
+
+
+def map_model(model):
+    if model == 'OLS':
+        return 'Линейная модель'
+    elif model == 'Polynomial':
+        return 'Полиномиальная модель'
+    return model
 
 
 def delete_model(model_id):
