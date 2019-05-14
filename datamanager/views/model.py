b26c025b35fb47eb5a3d3fd1ea3da165007e14fd
Anton Glushkov <alexandrovich.anton7@gmail.com>
2019-05-14 06:44:33 +0400

    added models owner check

--- datamanager/views/model.py
+++ datamanager/views/model.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render_to_response, redirect
 from django.views import generic
 
 from datamanager.models import MlModel
@@ -10,12 +11,21 @@ class ModelView(LoginRequiredMixin, generic.TemplateView):
     redirect_field_name = None
     template_name = 'model.html'
 
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-        model = MlModel.objects.get(pk=kwargs.get('model_id'))
-        data['model'] = model
+    @staticmethod
+    def get(request, model_id):
+        args = {}
+        try:
+            model = MlModel.objects.get(pk=model_id)
+        except:
+            return redirect('/data/models/', request)
 
-        data['meta'] = get_columns_meta(model.dataset.id, model.ds_in_cols) + \
+        if model.author.id != request.user.id:
+            return redirect('/data/models/', request)
+
+        args['model'] = model
+
+        args['meta'] = get_columns_meta(model.dataset.id, model.ds_in_cols) + \
                        get_columns_meta(model.dataset.id, model.ds_out_cols)
-        data['activation'] = func_mapping.get(model.activation, '')
-        return data
+        args['activation'] = func_mapping.get(model.activation, '')
+
+        return render_to_response('model.html', args)
